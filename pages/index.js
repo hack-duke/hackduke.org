fe5f616dd291938d10eb2555d506f9b4d4afad97
Yihong Song <ys377@duke.edu>
2022-12-28 03:10:51 -0500

    Add First Section to Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Navbar from "../components/Navbar";
 import HeroBackground from "../components/HeroBackground";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
     return (
@@ -11,6 +13,81 @@ export default function Home() {
                     <img src="/images/hd-logo.svg" />
                 </div>
             </div>
+            <section className="section is-small">
+                <div className="container">
+                    <div className="columns is-desktop">
+                        <div className="column" id="index-s1-c1">
+                            <div className="subtitle">
+                                We are a tech organization that brings together
+                                a multidisciplinary community at Duke to further
+                                social good. Sound interesting?
+                            </div>
+                            <div
+                                className="button"
+                                id="index-s1-b1"
+                                style={{ color: "#0042c6" }}>
+                                Read our story{" "}
+                                <FontAwesomeIcon
+                                    className="fa-md faicon"
+                                    icon={faArrowRight}></FontAwesomeIcon>
+                            </div>
+                        </div>
+                        <div className="column" id="index-s1-c2">
+                            <img id="index-s1-i1" src="/graphics/group1.svg" />
+                        </div>
+                    </div>
+                    <div className="columns is-desktop" id="index-s2">
+                        <div
+                            className="column"
+                            id="index-s2-c1"
+                            style={{ "border-color": "#36c69b" }}>
+                            <img
+                                className="card-image"
+                                src="/graphics/community.svg"
+                            />
+                            <div className="card-title">Community</div>
+                            <div className="card-text">
+                                We believe in fostering an internal, university,
+                                and local tech community that celebrates
+                                collaboration, diversity, and social impact.
+                            </div>
+                        </div>
+                        <div
+                            className="column"
+                            id="index-s2-c2"
+                            style={{ "border-color": "#f7af1d" }}>
+                            <img
+                                className="card-image"
+                                src="/graphics/innovation.svg"
+                            />
+                            <div className="card-title">Innovation</div>
+                            <div className="card-text">
+                                With our diverse skillsets and interests, we
+                                create extraordinary things that inspire others
+                                to create something cool. Let’s build!
+                            </div>
+                        </div>
+                        <div
+                            className="column"
+                            id="index-s3-c2"
+                            style={{ "border-color": "#e55511" }}>
+                            <img
+                                className="card-image"
+                                src="/graphics/social.svg"
+                            />
+                            <div className="card-title">Social Good</div>
+                            <div className="card-text">
+                                It’s not just about meaningful projects. We love
+                                to discuss, share, and bring to life ideas that
+                                aim to make a positive impact on social issues.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            {/* <section className="section is-small">
+                <div className="container"></div>
+            </section> */}
         </>
     );
 }
