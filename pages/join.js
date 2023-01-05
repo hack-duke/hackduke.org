bd9b8ccae854be9da4c22bbfd7942dc135fd9a87
Yihong Song <ys377@duke.edu>
2023-01-04 19:06:15 -0500

    Add Shortened Recruitment Page

--- pages/join.js
+++ pages/join.js
@@ -8,11 +8,20 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 export default function Join() {
     return (
         <>
-            <Navbar />
+            <Navbar color="#0042c6" />
             <div className="join">
                 <div className="hero">
                     <HeroBackground />
-                    <div>Join Us</div>
+                    <div id="join-hero">
+                        <div className="container">
+                            <div className="section-title">Join Our Team!</div>
+                            <div className="subtitle">
+                                Thanks for your interest in joining HackDuke!
+                                Our next recruiting cycle starts Spring 2023.
+                                More information will be released soon.
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer />
