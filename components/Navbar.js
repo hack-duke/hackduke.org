1394173e23cc1867ebff63abcf0ddc21b1f988fd
Arvindh Manian <arvindh.manian@gmail.com>
2023-03-03 16:50:04 -0500

    fix spacing to conform to codebase

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,89 +2,90 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 
 const NavbarItem = ({ text, URL, router }) => {
-  return (
-    <div
-      className="navbar-item navbar-button mr-4 transform font-sans text-[1.2rem] text-white before:absolute before:bottom-1 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:rounded before:bg-white before:transition-transform before:duration-300 before:ease-in-out before:content-[''] hover:cursor-pointer"
-      onClick={() => {
-        router.push(URL);
-      }}
-    >
-      {text}
-    </div>
-  );
+    return (
+        <div
+            className="navbar-item navbar-button mr-4 transform font-sans text-[1.2rem] text-white before:absolute before:bottom-1 before:left-0 before:h-0.5 before:w-full before:origin-right before:scale-x-0 before:rounded before:bg-white before:transition-transform before:duration-300 before:ease-in-out before:content-[''] hover:cursor-pointer"
+            onClick={() => {
+                router.push(URL);
+            }}
+        >
+            {text}
+        </div>
+    );
 };
 
 function Navbar(props) {
-  const router = useRouter();
-  const [isExpanded, toggleExpansion] = useState(false);
+    const router = useRouter();
+    const [isExpanded, toggleExpansion] = useState(false);
 
-  return (
-    <>
-      <nav
-        className="navbar absolute w-full bg-transparent px-4 py-8"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div className="navbar-brand ml-8">
-          <div
-            className="navbar-item mr-4 scale-[1.30] font-sans text-[1.2rem] text-white hover:cursor-pointer"
-            onClick={() => {
-              router.push("/");
-            }}
-          >
-            <img id="hd-logo" src="/images/hd-symbol.svg" />
-          </div>
-          <a
-            role="button"
-            className={
-              "navbar-burger scale-[1.30] text-white hover:bg-transparent hover:text-white" +
-              (isExpanded ? " is-active" : "")
-            }
-            onClick={() => {
-              toggleExpansion(!isExpanded);
-            }}
-            aria-label="menu"
-            aria-expanded="false"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
+    return (
+        <>
+            <nav
+                className="navbar absolute w-full bg-transparent px-4 py-8"
+                role="navigation"
+                aria-label="main navigation"
+            >
+                <div className="navbar-brand ml-8">
+                    <div
+                        className="navbar-item mr-4 scale-[1.30] font-sans text-[1.2rem] text-white hover:cursor-pointer"
+                        onClick={() => {
+                            router.push("/");
+                        }}
+                    >
+                        <img id="hd-logo" src="/images/hd-symbol.svg" />
+                    </div>
+                    <a
+                        role="button"
+                        className={
+                            "navbar-burger scale-[1.30] text-white hover:bg-transparent hover:text-white" +
+                            (isExpanded ? " is-active" : "")
+                        }
+                        onClick={() => {
+                            toggleExpansion(!isExpanded);
+                        }}
+                        aria-label="menu"
+                        aria-expanded="false"
+                    >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
 
-        <div
-          className={
-            "navbar-menu bg-transparent" + (isExpanded ? " is-active" : "")
-          }
-          // style background color
-          style={{ backgroundColor: props.color }}
-        >
-          <div className="navbar-end mr-8">
-            <NavbarItem text="About" url="/about" router={router}>
-              {" "}
-            </NavbarItem>
-            <NavbarItem text="Humans" url="/humans" router={router}>
-              {" "}
-            </NavbarItem>
-            <NavbarItem text="Events" url="/events" router={router}>
-              {" "}
-            </NavbarItem>
-            <NavbarItem text="Join Us" url="/join" router={router}>
-              {" "}
-            </NavbarItem>
+                <div
+                    className={
+                        "navbar-menu bg-transparent" +
+                        (isExpanded ? " is-active" : "")
+                    }
+                    // style background color
+                    style={{ backgroundColor: props.color }}
+                >
+                    <div className="navbar-end mr-8">
+                        <NavbarItem text="About" url="/about" router={router}>
+                            {" "}
+                        </NavbarItem>
+                        <NavbarItem text="Humans" url="/humans" router={router}>
+                            {" "}
+                        </NavbarItem>
+                        <NavbarItem text="Events" url="/events" router={router}>
+                            {" "}
+                        </NavbarItem>
+                        <NavbarItem text="Join Us" url="/join" router={router}>
+                            {" "}
+                        </NavbarItem>
 
-            {/* <div
+                        {/* <div
                             className="navbar-item navbar-button"
                             onClick={() => {
                                 router.push("/");
                             }}>
                             Login
                         </div> */}
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
