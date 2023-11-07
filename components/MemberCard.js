83e0d2a43a52c0b067a5fb3c9a768f57571e3de3
James Xu <james@jamesxu.ca>
2023-11-06 19:33:07 -0500

    new members and JXL/AVIF team images

--- components/MemberCard.js
+++ components/MemberCard.js
@@ -4,24 +4,53 @@ function MemberCard(props) {
     return (
         <div className="column is-one-fifth-desktop is-one-third-tablet is-half-mobile member-card">
             <a
-                className="card-image w-full rounded-2xl bg-center bg-no-repeat brightness-100 transition-all duration-500 after:block after:pb-[100%] after:content-['']  hover:brightness-[80%] hover:transition-all hover:duration-500"
-                style={{
-                    backgroundImage:
-                        "url('" +
-                        props.src +
-                        "'), linear-gradient(45deg, #eef2f3, #8e9eab)",
-                    backgroundSize: "cover",
-                }}
+                className="card-image hover:brightness-80 w-full rounded-2xl brightness-100 transition-all duration-500"
                 href={props.linkedin}
                 target="_blank"
                 rel="noopener noreferrer"
             >
+                <picture>
+                    <source
+                        srcset={props.src + ".jxl"}
+                        type="image/jxl"
+                    ></source>
+                    <source
+                        srcset={props.src + ".avif"}
+                        type="image/avif"
+                    ></source>
+                    <img
+                        src={props.src + ".jpeg"}
+                        className="hover:brightness-80"
+                        alt="Image"
+                        style={{
+                            objectFit: "cover",
+                            width: "100%",
+                            height: "100%",
+                            borderRadius: "1rem",
+                            aspectRatio: "1",
+                        }}
+                    />
+                </picture>
+                <div
+                    className="overlay"
+                    style={{
+                        background: "linear-gradient(45deg, #eef2f3, #8e9eab)",
+                        position: "absolute",
+                        top: "0",
+                        left: "0",
+                        width: "100%",
+                        height: "100%",
+                        opacity: "0",
+                        transition: "opacity 0.5s",
+                    }}
+                ></div>
                 {/* LinkedIn Icon for mobile/tablet screen sizes */}
                 <FaLinkedin
                     className="faicon has-text-white absolute right-2.5 bottom-2.5"
                     style={{ fontSize: "1.75rem" }}
                 />
             </a>
+
             <div className="card-content mt-2 mb-4 p-0">
                 <div className="text-[1.5rem] font-normal">{props.name}</div>
                 <div>{props.major}</div>
--- components/TeamCards.js
+++ components/TeamCards.js
@@ -30,7 +30,7 @@ function TeamCards(props) {
         <div className="columns is-mobile is-multiline">
             {teamMembers.map((t) => (
                 <MemberCard
-                    src={"/images/team-2023/" + t.First + t.Last + ".webp"}
+                    src={"/images/team-2023/" + t.First + t.Last}
                     name={t.First + " " + t.Last}
                     title={t.Title}
                     major={t.Major}
