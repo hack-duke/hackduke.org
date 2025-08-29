fca02895d0a47d8eb0431c61f36a4a8b5c58d03d
Derek Wang <xiran.wang@duke.edu>
2025-08-29 10:18:28 -0400

    random space

--- pages/join.js
+++ pages/join.js
@@ -62,7 +62,7 @@ export default function Join() {
                         </div>
                     </div>
                 </div>
-            </div >
+            </div>
             <Footer />
         </>
     );
